94c72f0f0a2d5a64099f947bf89b488aa544f6e7
vijay6776 <vijay6776.vtalent@gmail.com>
2024-11-20 22:34:09 +0530

    Anconda first commit

---
+++ src/com/python/Cobra.java
@@ -0,0 +1,18 @@
+package com.python;
+
+public class Cobra {
+
+	private String name;
+	private String colour;
+	private Double length;
+	private Double weight;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ src/com/python/Music.java
@@ -0,0 +1,11 @@
+package com.python;
+
+public class Music {
+	
+	public static void main(String[] args) {
+		Cobra cobra=new Cobra();
+		cobra.setName("black");
+		System.out.println(cobra.getName());
+	}
+
+}
